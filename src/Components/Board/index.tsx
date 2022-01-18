7f7dd3e1d73fb81227f1def8ed08a417320c2426
Murilo Duarte <duartemurilom@gmail.com>
2022-01-18 13:56:59 -0300

    First commit.

---
+++ src/Components/Board/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Board() {
+  return (
+    <div>
+      <img src="../../../public/Assets/tileset.gif" alt="board" />
+    </div>
+  )
+}
+
+export default Board
